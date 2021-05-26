7afde1bb91ddcab81327618634a0737631e94f07
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-05-26 11:21:41 -0300

    Cities Listing

--- II_Bimestre/Desafio_I/main.py
+++ II_Bimestre/Desafio_I/main.py
@@ -3,6 +3,7 @@ Escola Técnica Estadual Porto Digital
 Data: 21/05/21
 Tema: Problemas dentários no Brasil - Case de estudo em Alagoas
 '''
+from pandas.core.accessor import CachedAccessor
 import dados
 
 #! Criação de uma lista
@@ -16,3 +17,6 @@ print(len(cidades))
 
 # Acessar um índice específico
 # Neste caso, é limitar a exposição de dados a uma cidade
+print("-=-=-=-=- Lista cidades -=-=-=-=-")
+for i in cidades:
+    print(i[0], flush=True)
