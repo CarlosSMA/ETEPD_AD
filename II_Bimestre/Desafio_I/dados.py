91153aca4911c03efb86cf3d98cd2f7f38d7d110
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-05-26 11:06:08 -0300

    Added main.py + initial scripts

--- II_Bimestre/Desafio_I/dados.py
+++ II_Bimestre/Desafio_I/dados.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # Importando as bibliotecas necessárias para o desenvolvimento da análise de dados
 
-df = pd.read_csv(r"/home/jorge/Documentos/GitHub/ETEPD_AD/II_Bimestre/Desafio_I/dentistasphabitante2011.csv")
+df = pd.read_csv(r"dentistasphabitante2011.csv")
 # Importando lista de dados de uma tabela.
 
 valores = df.values
@@ -12,7 +12,6 @@ colunas = df.columns
 # df.Municípios = df.Municípios.astype("category")
 
 maiores = df.query('Dentistasphabitante > 0.85').head(10)
-print(maiores)
 # Selecionando os Municípios com as maiores taxas de dentista por mil habitantes
 
 plt.figure(figsize=(16, 12))
@@ -21,10 +20,12 @@ plt.bar(maiores["Municípios"], maiores["Dentistasphabitante"])
 # plt.plot(maiores["Municípios"], maiores["Dentistasphabitante"], "o")
 # Gŕafico de pontos(Figure_2)
 
-plt.xlabel("Munícipios")
+plt.xlabel("Municípios")
 plt.ylabel("Dentistas por mil habitantes")
-plt.title("Dentistas por habitantes no estado de Alagoas")
+plt.title("Dentistas por habitantes no estado de Alagoas em 2011")
 # Gerando as etiquetas do gráfico.
 
-plt.show()
+def mostrar_graficos():
+    plt.show()
+    print(maiores)
 # Mostrando o gráfico
--- II_Bimestre/Desafio_I/main.py
+++ II_Bimestre/Desafio_I/main.py
@@ -1,11 +1,18 @@
 '''
 Escola Técnica Estadual Porto Digital
 Data: 21/05/21
-Tema: Problemas dentários
+Tema: Problemas dentários no Brasil - Case de estudo em Alagoas
 '''
-from random import randint
-i = 0
-j = 0
-# Carlos
-possiveis_problemas_list = ['carie', 'gengivite', 'placa', 'afta'] 
-id_pacientes = list(range(20))
+import dados
+
+#! Criação de uma lista
+cidades = list(dados.valores)
+
+#! -=-=-=-=-=-=-=-=- MÉTODOS DO TIPO "LISTA" -=-=-=-=-=-=-=-=-
+
+# len -> Tamanho (nº. de índices)
+# Neste caso, o nº. de índices representa a quantidade de cidades avaliadas
+print(len(cidades))
+
+# Acessar um índice específico
+# Neste caso, é limitar a exposição de dados a uma cidade
