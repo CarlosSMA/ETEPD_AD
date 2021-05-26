4dc8319d1f8b129efb449f78620c503c1251d194
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-05-26 11:46:51 -0300

    Estending list method added

--- II_Bimestre/Desafio_I/main.py
+++ II_Bimestre/Desafio_I/main.py
@@ -3,7 +3,6 @@ Escola Técnica Estadual Porto Digital
 Data: 21/05/21
 Tema: Problemas dentários no Brasil - Case de estudo em Alagoas
 '''
-from pandas.core.accessor import CachedAccessor
 import dados
 from time import sleep
 
@@ -12,21 +11,27 @@ cidades = list(dados.valores)
 
 #! -=-=-=-=-=-=-=-=- MÉTODOS DO TIPO "LISTA" -=-=-=-=-=-=-=-=-
 
-# len -> Tamanho (nº. de índices)
+#* Tamanho de uma lista
+#  len -> Tamanho (nº. de índices)
 # Neste caso, o nº. de índices representa a quantidade de cidades avaliadas
-print(len(cidades))
-sleep(2)
 
-# Acessar um índice específico
+print(f"Nº. de cidades: {len(cidades)}", flush=True)
+sleep(4)
+
+#* Acessar um índice específico
 # Neste caso, é limitar a exposição de dados a uma cidade
 print("-=-=-=-=- Lista cidades -=-=-=-=-")
 for i in cidades:
     print(f"- {i[0]}")
 
-sleep(2)
+sleep(4)
 
-# Acessar um range numa lista
+#* Acessar um range numa lista
 # Neste exemplo, há o acesso das 4 primeiras cidades
 print(cidades[:4])
-sleep(2)
+sleep(4)
 
+#* Estender uma lista
+novas_cidades = [["Cidade Teste", 0.7]]
+cidades.extend(novas_cidades)
+print(cidades[-1])
