d8f1e74eaf10580f96ba7995a4a88854ad2a4716
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-05-27 14:42:27 -0300

    Printing added

--- II_Bimestre/Desafio_I/main.py
+++ II_Bimestre/Desafio_I/main.py
@@ -14,27 +14,35 @@ cidades = list(dados.valores)
 #* Tamanho de uma lista
 #  len -> Tamanho (nº. de índices)
 # Neste caso, o nº. de índices representa a quantidade de cidades avaliadas
-
+print('1 - Utilizando o método "len" numa lista')
+sleep(2)
 print(f"Nº. de cidades: {len(cidades)}", flush=True)
 sleep(4)
 
 #* Acessar um índice específico
 # Neste caso, é limitar a exposição de dados a uma cidade
+print('\n2 - Printando os elementos da lista')
+sleep(3)
 print("-=-=-=-=- Lista cidades -=-=-=-=-")
 for i in cidades:
     print(f"- {i[0]}")
 
-sleep(4)
+sleep(3)
 
 #* Acessar um range numa lista
 # Neste exemplo, há o acesso das 4 primeiras cidades
+print('\n3 - Printando um range numa lista')
+sleep(2)
 print(cidades[:4])
 sleep(4)
 
 #* Estender uma lista
+print('\n4 - Estendendo uma lista')
+sleep(2)
 novas_cidades = [["Cidade Teste", 0.7]]
 cidades.extend(novas_cidades)
 print(cidades[-1])
 
+print('\n5 - Mostrando os gráficos')
+sleep(2)
 dados.mostrar_graficos()
-
