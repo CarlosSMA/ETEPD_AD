db453089c252f3f4739322bdbe9f5b58b903c109
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-08-21 13:47:47 -0300

    fix: arquivo db_bom_dente movido para a pasta db

--- III_Bimestre/OPDA/main.py
+++ III_Bimestre/OPDA/main.py
@@ -10,12 +10,12 @@ Turma: 3ºA
 import psycopg2
 
 # Leitura do arquivo .sql exportado a partir da plataforma https://dbdiagram.io
-db_bom_dente_temp = open("db_bom_dente.sql")
+db_bom_dente_temp = open("db/db_bom_dente.sql")
 db_bom_dente = db_bom_dente_temp.read()
 
 comandos = db_bom_dente.split(';')
 print(comandos)
 
 # Loop para executar os comandos do script (criação de tabelas, ligação, etc)
-for execucao in comandos:
-    c.execute(comandos)
+# for execucao in comandos:
+    # c.execute(comandos)
