4b2b2fb8201f3f498a2b4b5e63a7876ead22a701
Carlos Alvarez <carloseduardosmalv@gmail.com>
2021-08-23 23:51:55 -0300

    feature: added full python script

--- III_Bimestre/OPDA/main.py
+++ III_Bimestre/OPDA/main.py
@@ -15,10 +15,29 @@ TODO
 import psycopg2
 
 #! Ligação com banco de dados
-conexao = psycopg2.connect(user="postgres", password="1234", host="0.0.0.0", port=5432)
+conexao = psycopg2.connect(database="consultorio", user="postgres", password="1234", host="0.0.0.0", port=5432)
 cursor = conexao.cursor()
 
 # Execução do script
 # Fonte: https://stackoverflow.com/questions/17261061/execute-sql-schema-in-psycopg2-in-python
 # Leitura do arquivo .sql exportado a partir da plataforma https://dbdiagram.io
-cursor.execute(open("db/db_bom_dente.sql", "r").read())
+
+#cursor.execute(open("db/db_bom_dente.sql").read())
+conexao.commit()
+
+
+while True:
+    comando = input("Insira um comando\n> ")
+    try:
+        cursor.execute(comando)
+        pass
+    except psycopg2.errors.SyntaxError:
+        print(f"Comando \"{comando}\" inválido.")
+        pass
+
+    conexao.commit()
+    if comando == "quit":
+        break
+
+cursor.close()
+conexao.close()
